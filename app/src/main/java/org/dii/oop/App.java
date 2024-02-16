264dc357ee0044fa8b6122a40be36f4f03a22356
Taowa <jirawat_song@cmu.ac.th>
2024-02-16 12:16:08 +0700

    add working dir

--- app/src/main/java/org/dii/oop/App.java
+++ app/src/main/java/org/dii/oop/App.java
@@ -1,4 +1,4 @@
-package main.java.org.dii.oop.lesson06.exercise01;
+package main.java.org.dii.oop.lesson06.exercise02;
 public class App {
     public static void main(String[] args){
         Lesson.run();
